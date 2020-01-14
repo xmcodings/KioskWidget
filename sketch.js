105d85a6aa3ab446ba3c5d183fdf963d5043b668
xmcodings <xmuniv@cau.ac.kr>
2020-01-14 21:25:31 +0900

    sssss

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function setup()
 createCanvas(windowWidth, windowHeight); 
 
 
-
+tempBut = new WidgetButton();
 background(20,40,40);
 img = loadImage("resources/temp.jpg");
 
@@ -18,12 +18,30 @@ weight = 3;
 
 function draw()
 {
-
-  text('마우스 위치', mouseX-50,mouseY);
-
   image(img, 0, 0, windowWidth, windowHeight);
+  tempBut.move();
+  tempBut.display();
+
+  
 }
 
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
+class WidgetButton {
+  constructor() {
+    this.x = random(windowWidth);
+    this.y = random(windowHeight);
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
 
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  display() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+
+}
