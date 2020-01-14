046e2372614eb4f31dbf24c3aa1569b35382d07f
xmcodings <xmuniv@cau.ac.kr>
2020-01-14 20:36:21 +0900

    showtempimage

--- sketch.js
+++ sketch.js
@@ -1,33 +1,19 @@
 var weight;
-
+let img;
 function setup()
 {
-createCanvas(500,500);
+createCanvas(windowWidth, windowHeight); 
 background(20,40,40);
 
 strokeWeight(10);
-
-
-triangle(100,9,110,100,85,100);
-
-quad(20,20,20,70,60,90,60,40);
-quad(200,400,350,410,410,90,60,40);
-
-rect(15,15,40,60);
-
-ellipse(40,40,60,60);
+img = loadImage('resources/temp.jpg');
 
 weight = 3;
 }
 
 function draw()
 {
-  if(mouseIsPressed)
-  {
-    background(0, 20);
-  }else{
-  background(0, 50);
-}
+  
   strokeWeight(weight);
 
   stroke(255,0,0);
@@ -45,30 +31,6 @@ function draw()
   textSize(30);
 
   text('마우스 위치', mouseX-50,mouseY);
-/*
-  if(mouseIsPressed)
-  {
-    if(mouseButton == LEFT)
-    {
-      weight++;
-    }
-    if(mouseButton == RIGHT)
-    {
-      weight--;
-    }
-  }
-*/
+  image(img, 0, 0, windowWidth, windowHeight);
 }
-
-function mouseWheel(event) {
-  print(event.delta);
-  //move the square according to the vertical scroll amount
-  if(event.delta > 0)
-  {
-    weight ++;
-  }
-  if(event.delta < 0)
-  {
-    weight --;
-  }
-}
+function windowResized() { resizeCanvas(windowWidth, windowHeight); }
