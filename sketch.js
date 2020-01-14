8be6b91c925ddc69998af4bd165c239bb7aec547
daust05 <55437034+daust05@users.noreply.github.com>
2020-01-15 03:06:47 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,15 +31,23 @@ class Widget {
       this.update();
       strokeWeight(3);
       if(this.count){
-        c = color('rgba(0,0,0,0.5)');
+        c = color('rgba(255,255,255, 1)');
         fill(c);
         arc(windowWidth-60, this.widget_height, 200, 200, PI, PI + QUARTER_PI,PIE);
         arc(windowWidth-60, this.widget_height, 200, 200, PI-QUARTER_PI, PI,PIE);
+
+        c = color('rgba(0,0,0,1)');
+        fill(c);
+        textSize(15);
+        text('차차모드',windowWidth-155,this.widget_height-20);
+        text('음성안내',windowWidth-155,this.widget_height+20);
+
+        c = color('rgba(255,255,255, 1');
       }
       else{
-        c = color('rgba(0,0,0, 0.25)');
-        fill(c);
+        c = color('rgba(255,255,255, 0.5)');
       }
+      fill(c);
       ellipse(windowWidth-60, this.widget_height, 50, 50);
       image(icon,windowWidth-82,this.widget_height-12,40,20);
     }
