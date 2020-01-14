872c85f10ad19e2d6fdb6c09a0821af84b8e8b4f
xmcodings <xmuniv@cau.ac.kr>
2020-01-14 20:43:28 +0900

    jininin

--- sketch.js
+++ sketch.js
@@ -1,36 +1,23 @@
 var weight;
 let img;
+
+
 function setup()
 {
+  
 createCanvas(windowWidth, windowHeight); 
 background(20,40,40);
-
-strokeWeight(10);
 img = loadImage('resources/temp.jpg');
+strokeWeight(10);
 
 weight = 3;
 }
 
 function draw()
 {
-  
-  strokeWeight(weight);
-
-  stroke(255,0,0);
-  triangle(mouseX, mouseY,110,100,200,100);
-
-  strokeWeight(weight);
-
-  stroke(20,241,20);
-  triangle(mouseY, mouseX,400,320,160,140);
-
-
-  stroke(200,0,0);
-  fill(255);
-  strokeWeight(weight);
-  textSize(30);
 
   text('마우스 위치', mouseX-50,mouseY);
+
   image(img, 0, 0, windowWidth, windowHeight);
 }
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
