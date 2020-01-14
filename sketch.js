4d3dcd3406f4aa5cae5115b298873ef05a647383
daust05 <55437034+daust05@users.noreply.github.com>
2020-01-14 22:08:31 +0900

    widgetplus

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var weight;
 let img;
+let icon;
 
 
 function setup()
@@ -8,14 +9,15 @@ function setup()
 createCanvas(windowWidth, windowHeight); 
 background(20,40,40);
 img = loadImage('resources/temp.jpg');
-strokeWeight(10);
-
-weight = 3;
+icon = loadImage('resources/WidgetIcon.png');
 }
 
 function draw()
 {
+  fill('rgba(0,0,0, 0.25)');
+  strokeWeight(3);
   image(img, 0, 0, windowWidth, windowHeight);
   ellipse(windowWidth-60, mouseY, 50, 50);
+  image(icon,windowWidth-82,mouseY-12,40,20);
 }
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
