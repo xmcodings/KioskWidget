1a0353f66e2129634dcb227bfbacca12e16a6676
Choi <cjsrole@gmail.com>
2020-01-14 22:20:04 +0900

    3.51

--- sketch.js
+++ sketch.js
@@ -9,15 +9,6 @@ function setup()
 createCanvas(windowWidth, windowHeight); 
 
 background(20,40,40);
-img = loadImage('resources/temp.jpg');
-strokeWeight(10);
-
-tempBut = new WidgetButton();
-background(20,40,40);
-img = loadImage("resources/temp.jpg");
-
-
-background(20,40,40);
 img = loadImage("resources/temp.jpg");
 imageRatio = img.height / img.width;
 weight = 3;
@@ -26,7 +17,6 @@ realHeight = windowHeight;
 
 function draw()
 {
-
   var imgWidth;
   var imgHeight;
   imageWidth = img.width;
@@ -48,34 +38,4 @@ function draw()
 
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
-
-  image(img, 0, 0, windowWidth, windowHeight);
-
-  ellipse(windowWidth-60, mouseY, 50, 50);
-
-  tempBut.move();
-  tempBut.display();
-
-
-function windowResized() { resizeCanvas(windowWidth, windowHeight); }
-
-class WidgetButton {
-  constructor() {
-    this.x = random(windowWidth);
-    this.y = random(windowHeight);
-    this.diameter = random(10, 30);
-    this.speed = 1;
-  }
-
-  move() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  }
-
-  display() {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-  }
-
-}
-function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
